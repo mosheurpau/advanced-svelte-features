9096d7d97b3c11dcdad94b2158aa00e6f93ba325
mosheurpau <mosheur.pau@gamil.com>
2024-06-22 12:04:22 +0600

    Creating Reusable Components MyButton

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Button from "./components/MyButton.svelte";
+
+  let count = 0;
+</script>
+
+<h2>Creating Reusable Components</h2>
+<p>{count}</p>
+<!-- Ensure the parent component properly listens for the myEvent and updates the count state. -->
+<Button on:myEvent={() => count++} text="Increment Button" />
+<Button on:myEvent={() => count--} text="Decrement Button" />
+<Button text="Test" />
+<Button text="Using the Button Component" />
+<Button text="Reusable Button" />
+<Button />
---
+++ src/components/MyButton.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let text = "Click me";
+
+  //   Update the Button.svelte file to dispatch a click event.
+  const dispatch = createEventDispatcher();
+  function handleClick() {
+    dispatch("myEvent");
+  }
+</script>
+
+<!-- Creating a Button Component  -->
+<button on:click={handleClick}>{text}</button>
+
+<style>
+  button {
+    padding: 10px;
+    background-color: blue;
+    color: white;
+    border: none;
+    border-radius: 5px;
+  }
+</style>
