bfe5c5b1b38df4940be0349d1ab4b9895436110b
mosheurpau <mosheur.pau@gamil.com>
2024-06-22 15:54:55 +0600

    added Writable Store and Readable stores for state management

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,33 @@
 <script>
   import Button from "./components/MyButton.svelte";
+  import { countStore } from "./stores/writableStore.js";
+  import { time } from "./stores/readableStore";
+  let countsNumer = 0;
 
-  let count = 0;
+  // Writable Store
+  function increment() {
+    countStore.update((n) => n + 2);
+  }
 </script>
 
 <h2>Creating Reusable Components</h2>
-<p>{count}</p>
+<p>{countsNumer}</p>
 <!-- Ensure the parent component properly listens for the myEvent and updates the count state. -->
-<Button on:myEvent={() => count++} text="Increment Button" />
-<Button on:myEvent={() => count--} text="Decrement Button" />
+<Button on:myEvent={() => countsNumer++} text="Increment Button" />
+<Button on:myEvent={() => countsNumer--} text="Decrement Button" />
 <Button text="Test" />
-<Button text="Using the Button Component" />
-<Button text="Reusable Button" />
-<Button />
+
+<br /> <br /> <br />
+
+<h2>------ Stores for State Management ------</h2>
+<h3>1. Writable Store</h3>
+<button on:click={increment}>Increment 2 count Store: {$countStore}</button>
+<button on:click={() => countStore.update((c) => c + 1)}
+  >Increment 1 count Store: {$countStore}</button
+>
+
+<br /><br /> <br />
+
+<h3>2. Readable Store</h3>
+<p>Current time: {$time}</p>
+<p>Current time: {$time.toLocaleTimeString()}</p>
